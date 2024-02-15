f4caf3143ba8bd4a82b88b728fa426f687a4875f
Fernando-crz <fernando.cruz.ctt@gmail.com>
2024-02-14 21:52:26 -0300

    add support for attention visualization

--- evaluation.py
+++ evaluation.py
@@ -2,6 +2,10 @@ import torch
 from PIL import Image
 from models import *
 from torchvision.transforms import Resize, CenterCrop, Compose, ToTensor
+from torchvision.transforms.functional import to_pil_image
+from matplotlib import cm
+from matplotlib import pyplot as plt
+from math import ceil
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -30,22 +34,22 @@ def caption_image(encoder, decoder, image_path, vocab, beam_size=3):
 
     bos_token = torch.tensor([vocab["<bos>"]]).to(device)
     hidden, cell = decoder._init_hidden_and_cell_state(1)
-    top_k_scores, top_k_tokens, hidden, cell = get_top_k_predictions(decoder, encoded_image, bos_token, hidden, cell, beam_size)
+    top_k_scores, top_k_tokens, hidden, cell, attention = get_top_k_predictions(decoder, encoded_image, bos_token, hidden, cell, beam_size)
     top_k_scores = top_k_scores.squeeze(0)
     top_k_tokens = top_k_tokens.squeeze(0)
     
-    best_candidates.extend((top_k_score, [top_k_token], hidden, cell) for top_k_score, top_k_token in zip(top_k_scores, top_k_tokens))
+    best_candidates.extend((top_k_score, [top_k_token], hidden, cell, [attention]) for top_k_score, top_k_token in zip(top_k_scores, top_k_tokens))
 
     it = 1
     while beam_size > 0:
         possible_candidates = []
         for candidate in best_candidates:
-            score, tokens, hidden, cell = candidate
-            top_k_scores, top_k_tokens, hidden, cell = get_top_k_predictions(decoder, encoded_image, tokens[-1].unsqueeze(0), hidden, cell, beam_size)
+            score, tokens, hidden, cell, attention_list = candidate
+            top_k_scores, top_k_tokens, hidden, cell, attention = get_top_k_predictions(decoder, encoded_image, tokens[-1].unsqueeze(0), hidden, cell, beam_size)
             top_k_scores = top_k_scores.squeeze(0)
             top_k_tokens = top_k_tokens.squeeze(0)
 
-            possible_candidates.extend((score + top_k_score, tokens + [top_k_token], hidden, cell) for top_k_score, top_k_token in zip(top_k_scores, top_k_tokens))
+            possible_candidates.extend((score + top_k_score, tokens + [top_k_token], hidden, cell, attention_list + [attention]) for top_k_score, top_k_token in zip(top_k_scores, top_k_tokens))
         best_candidates = sorted(possible_candidates, key=lambda x: x[0], reverse=True)[:beam_size]
 
         for candidate in best_candidates:
@@ -54,20 +58,53 @@ def caption_image(encoder, decoder, image_path, vocab, beam_size=3):
                 best_candidates.remove(candidate)
                 beam_size -= 1
 
-    caption = sorted(final_candidates, key=lambda x: x[0], reverse=True)[0][1][:-1]
+    chosen_candidate = sorted(final_candidates, key=lambda x: x[0], reverse=True)[0]
+    caption = chosen_candidate[1][:-1]
+    attention_list = chosen_candidate[4][:-1]
 
-    return caption
+    return caption, attention_list
 
 @torch.no_grad()
 def get_top_k_predictions(decoder, encoded_image, token, hidden, cell, k):
-    context, _ = decoder.attention(encoded_image, hidden)
-
+    context, attention = decoder.attention(encoded_image, hidden)
     embeded_caption = decoder.embedding(token)
     hidden, cell = decoder.lstm(torch.cat((context, embeded_caption), dim=1), (hidden, cell))
     preds = decoder.lstm_fc(hidden) # (1, vocab_size)
 
     top_k_scores, top_k_tokens = torch.topk(preds, k)
-    return top_k_scores, top_k_tokens, hidden, cell
+    return top_k_scores, top_k_tokens, hidden, cell, attention
+
+def visualize_attention(encoder, decoder, image_path, vocab, beam_size=3):
+    caption, attention_list = caption_image(encoder, decoder, image_path, vocab, beam_size)
+    caption = [cap.item() for cap in caption]
+    caption = vocab.lookup_tokens(caption)
+
+    upsampler = torch.nn.Upsample(scale_factor=16, mode="bilinear")
+    attention_list = [upsampler(attention.view((1, 1, 14, 14))) for attention in attention_list]
+    attention_list = [to_pil_image(attention.squeeze(0)) for attention in attention_list]
+
+    image = Image.open(image_path)
+    image = img_transform(image).to(device)
+    image = to_pil_image(image)
+
+    plt.figure(figsize=(20, 12))
+    plt.subplot(ceil((len(caption) + 1) / 5), 5, 1)
+    plt.imshow(image)
+    plt.set_cmap(cm.Greys_r)
+    plt.axis("off")
+    
+    for i, (word, attention) in enumerate(zip(caption, attention_list)):
+        if i >= 50:
+            break
+        plt.subplot(ceil((len(caption) + 1) / 5), 5, i + 2)
+
+        plt.text(0, 1, str(word), color="black", backgroundcolor="white", fontsize=12)
+        plt.imshow(image)
+        plt.imshow(attention, alpha=0.7)
+        plt.set_cmap(cm.Greys_r)
+        plt.axis("off")
+    
+    plt.show()
 
 def main():
     print("device found: ", device)
@@ -80,7 +117,7 @@ def main():
     dropout = 0.4
 
     # Carregando modelos
-    checkpoint = torch.load("checkpoints/last.pth")
+    checkpoint = torch.load("checkpoints/best.pth")
     vocab = torch.load("checkpoints/vocab.pth")
     encoder = Encoder().to(device)
     decoder = Decoder(hidden_size, len(vocab), emb_size, attention_size, img_emb_size, num_channels, dropout).to(device)
@@ -89,9 +126,8 @@ def main():
 
     image_path = "coco/images/val2017/000000579635.jpg"
     beam_size = 3
-    caption = caption_image(encoder, decoder, image_path, vocab, beam_size)
-    caption = [cap.item() for cap in caption]
-    print(vocab.lookup_tokens(caption))
+    caption, _ = caption_image(encoder, decoder, image_path, vocab, beam_size)
+    visualize_attention(encoder, decoder, image_path, vocab, beam_size)
 
 if __name__ == "__main__":
     main()
